a11a214192b796b8003f2e999d1003edd94e2dc6
Rxtualz <peterhehe12@gmail.com>
2026-06-04 23:24:29 +0100

    test

---
+++ Source/AshlineDecay/AshlineDecay.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AshlineDecay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AshlineDecay, "AshlineDecay" );
